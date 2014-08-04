bc8da88c879a03e69aa5a8e0243163c710725de7
Elton Minetto <eminetto@gmail.com>
2014-08-04 16:48:01 -0300

    Service cache

--- src/Skel/Service/Service.php
+++ src/Skel/Service/Service.php
@@ -6,8 +6,16 @@ abstract class Service implements ServiceInterface
 {
 	protected $em;
 
+	protected $cache;
+
     public function setEm($em)
     {
         $this->em = $em;
     }
+
+    public function setCache($cache)
+	{
+		$this->cache = $cache;
+	}
+
 }
