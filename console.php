5ec68de525a1d19efee687664ea2596c704cec90
jesus Vieira <jesusvieiradelima@gmail.com>
2015-10-02 15:19:41 -0300

    migration configurada

--- console.php
+++ console.php
@@ -4,12 +4,13 @@ putenv('APPLICATION_ENV=development');
 
 $app = require_once __DIR__.'/bootstrao.php';
 
-use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
-use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
-use Symfony\Component\Console\Application as SymfonyApplication;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Symfony\Component\Console\Helper\HelperSet;
 use Symfony\Component\Console\Helper\DialogHelper;
+use Doctrine\DBAL\Migrations\Tools\Console\Command;
 use Doctrine\DBAL\Migrations\Configuration\Configuration;
+use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
+use Symfony\Component\Console\Application as SymfonyApplication;
 
 $helpers = array('dialog' => new DialogHelper());
 if (isset($app['orm.em'])) {
@@ -28,16 +29,16 @@ $commands = array(
 
 $application = new SymfonyApplication();
 $config = new Configuration($app['db']);
-$config->setName('Fillus Migration');
+$config->setName('Skel Migration');
 $config->setMigrationsDirectory(__DIR__.'/data/migrations');
-$config->setMigrationsNamespace('Fillus\\Migration');
+$config->setMigrationsNamespace('Skel\\Migration');
 $config->setMigrationsTableName('migration_version');
 
 foreach ($commands as $command) {
-    /** @var \Doctrine\DBAL\Migrations\Tools\Console\Command\AbstractCommand $command */
     if ($command instanceof Command\AbstractCommand){
         $command->setMigrationConfiguration($config);
     }
     $application->add($command);
 }
-return  ConsoleRunner::run($helperSet, $application->all());
+
+ConsoleRunner::run($helperSet, $application->all());
